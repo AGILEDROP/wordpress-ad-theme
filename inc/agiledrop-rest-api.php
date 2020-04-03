75a91d5983b0e96b23633d4a206ee7e230265daf
Ziga <ziga.koracin@agiledrop.com>
2020-04-03 14:55:30 +0200

    Working on gb.

--- inc/agiledrop-rest-api.php
+++ inc/agiledrop-rest-api.php
@@ -1,12 +1,12 @@
 <?php
 if ( !class_exists('Agiledrop_Rest ') ) {
-	class Agiledrop_Rest {
+	class Agiledrop_Rest extends WP_REST_Controller {
 		public function __construct() {
 			add_action( 'rest_api_init', array( $this, 'register_api' ) );
 		}
 
 		public function register_api() {
-
+			//Update for only backend with ajax
 			register_rest_route( 'agiledrop/v1', '/custom-post-types/', array(
 				'methods'   => 'GET',
 				'callback'  => array( $this, 'custom_post_types' )
@@ -17,7 +17,7 @@ if ( !class_exists('Agiledrop_Rest ') ) {
 				'callback'  => array( $this, 'all_hero_posts' )
 			));
 
-			register_rest_route( 'agiledrop/v1', '/jobs-posts/', array(
+			register_rest_route( 'agiledrop/v1', '/job-posts/', array(
 				'methods'   => 'GET',
 				'callback'  => array( $this, 'all_jobs_posts' )
 			));
@@ -25,7 +25,7 @@ if ( !class_exists('Agiledrop_Rest ') ) {
 		}
 
 		public function custom_post_types() {
-			return array( 'agiledrop-hero', 'agiledrop-jobs' );
+			return array( 'hero-posts', 'job-posts' );
 		}
 
 		public function all_hero_posts() {
@@ -36,7 +36,6 @@ if ( !class_exists('Agiledrop_Rest ') ) {
 					'numberposts'       => -1
 				)
 			);
-
 		}
 
 		public function all_jobs_posts() {
