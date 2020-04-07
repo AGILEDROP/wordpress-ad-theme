f62dc2c5920324070c24bb63c68c968054ada32c
Ziga <ziga.koracin@agiledrop.com>
2020-04-07 08:22:51 +0200

    Working on block still needs save function to update.

--- inc/agiledrop-rest-api.php
+++ inc/agiledrop-rest-api.php
@@ -1,53 +1,70 @@
 <?php
 if ( !class_exists('Agiledrop_Rest ') ) {
 	class Agiledrop_Rest extends WP_REST_Controller {
+
+		private $post_types = array( 'agiledrop-hero', 'agiledrop-jobs' );
+
 		public function __construct() {
 			add_action( 'rest_api_init', array( $this, 'register_api' ) );
 		}
 
 		public function register_api() {
-			//Update for only backend with ajax
+
 			register_rest_route( 'agiledrop/v1', '/custom-post-types/', array(
 				'methods'   => 'GET',
 				'callback'  => array( $this, 'custom_post_types' )
 			));
-
-			register_rest_route( 'agiledrop/v1', '/hero-posts/', array(
-				'methods'   => 'GET',
-				'callback'  => array( $this, 'all_hero_posts' )
-			));
-
-			register_rest_route( 'agiledrop/v1', '/job-posts/', array(
-				'methods'   => 'GET',
-				'callback'  => array( $this, 'all_jobs_posts' )
-			));
-
+			foreach ( $this->post_types as $type ) {
+				register_rest_route( 'agiledrop/v1', $type, array(
+					'methods'   => 'GET',
+					'callback'  => array( $this, 'return_posts'),
+					'args' => array( 'type' => $type ),
+				));
+			}
 		}
 
 		public function custom_post_types() {
-			return array( 'hero-posts', 'job-posts' );
+			return $this->post_types;
 		}
 
-		public function all_hero_posts() {
+		private function get_all_posts( $type ) {
 			return get_posts(
 				array(
-					'post_type'         => 'agiledrop-hero',
+					'post_type'         => $type,
 					'post_status'       => 'publish',
 					'numberposts'       => -1
 				)
 			);
 		}
 
-		public function all_jobs_posts() {
-			return get_posts(
-				array(
-					'post_type'         => 'agiledrop-jobs',
-					'post_status'       => 'publish',
-					'numberposts'       => -1
-				)
-			);
+		public function return_posts( $param ) {
+			$attributes = $param->get_attributes();
+			$type = $attributes['args']['type'];
+			$all_posts = [];
+			$posts = $this->get_all_posts( $type );
+			foreach ( $posts as $post ) {
+				$all_posts[$post->ID]['title'] = $post->post_title;
+				$all_posts[$post->ID]['image'] = $this->get_image( $post->post_content );
+				$all_posts[$post->ID]['text'] = $this->get_text( $post->post_content );
+				//$all_posts[$type][$post->ID]['link'] = $this->get_link( $post->post_content );
+			}
+			return $all_posts;
 		}
 
+		private function get_image( $content ) {
+			preg_match('/<img.+?src=[\'"]([^\'"]+)[\'"].*?>/i',$content, $match );
+			return $match[1];
+		}
+
+		private function get_text( $content ) {
+			preg_match("'<p>(.*?)</p>'si", $content, $match);
+			return $match[1];
+		}
+
+		private function get_link( $content ) {
+			preg_match("'<a>(.*?)</a>'si", $content, $match);
+			return $match[1];
+		}
 	}
 }
 
