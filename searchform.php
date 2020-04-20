db6cb4055715ff603517fa8b0e82f2377308373b
Ziga <ziga.koracin@agiledrop.com>
2020-04-20 10:01:53 +0200

    search form, for all post types.

--- searchform.php
+++ searchform.php
@@ -1,18 +1,16 @@
 
 <form id="agiledrop-search" method="get" action="<?php echo home_url('/'); ?>">
 	<fieldset>
-		<legend>Select where to search</legend>
-		<label for="taxonomies">Categories</label>
-		<input type="checkbox" name="taxonomies">
-		<label for="post-types">Post Types</label>
-		<input type="checkbox" name="post-types">
-		<input type="text" class="search-field" name="s" placeholder="Search" value="<?php the_search_query(); ?>">
-		<input type="submit" value="Search">
+        <input type="text" class="search-field" name="s" placeholder="Search" value="<?php the_search_query(); ?>">
+        <?php
+        $helper = new Agiledrop_Helper();
+        $post_types = $helper->get_all_post_types();
+        foreach ( $post_types as $type ) { ?>
+            <input type="hidden" name="post_type[]" value="<?php echo $type; ?>">
+        <?php } ?>
+        <input type="submit" value="Search">
 	</fieldset>
 </form>
 
 <?php
 
-$helper = new Agiledrop_Helper();
-$post_types = $helper->get_all_post_types();
-$b = 1;
