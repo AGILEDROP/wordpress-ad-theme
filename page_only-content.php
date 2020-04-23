f89439510a46082063993756efd4f9164ccfdce2
Martin Kenjic <martin.kenjic@agiledrop.com>
2020-04-23 15:55:02 +0200

    Add twocolumns style

--- page_only-content.php
+++ page_only-content.php
@@ -6,18 +6,13 @@ get_header();
 
 get_template_part( 'template-parts/partials/agiledrop-hero');
 ?>
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-            <div class="page-container container">
-                <div class="widget-content">
-                   <?php get_template_part( 'template-parts/partials/two-columns' );?>
-                </div>
-                <?php
-	            echo $post->post_content;
-	            ?>
-            </div>
-		</main>
-	</div>
-
+<div id="primary" class="content-area">
+	<main id="main" class="site-main">
+		<div class="widget-content">
+			<?php get_template_part( 'template-parts/partials/two-columns' );?>
+		</div>
+		<?php echo $post->post_content; ?>
+	</main>
+</div>
 <?php
 get_footer();
