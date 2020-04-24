67b8f27d31e8f07bbe8187225c0276fb3de29b4c
Martin Kenjic <martin.kenjic@agiledrop.com>
2020-04-24 10:06:36 +0200

    Fix hero responsive

--- template-parts/partials/agiledrop-hero.php
+++ template-parts/partials/agiledrop-hero.php
@@ -1,18 +1,20 @@
 <?php
-$helper = new Agiledrop_Helper();
-$hero = $helper->page_has_post( 'agiledrop-hero' );
- ?>
-<div class="hero">
-    <div class="hero__opacity"></div>
-    <div class="hero__background">
-        <figure class="hero__video">
-        <?php if ( !empty( $hero['background'] ) ) echo $hero['background']; ?>
-        </figure>
-    </div>
-    <div class="hero__text">
-        <div class="hero__container">
-            <h1 class="hero__heading"><?php if ( !empty( $hero['title'] ) ) echo $hero['title']; ?></h1>
-            <div class="hero__description"><?php if ( !empty( $hero['content'] ) ) echo $hero['content']; ?></div>
-        </div>
-    </div>
+	$helper = new Agiledrop_Helper();
+	$hero = $helper->page_has_post( 'agiledrop-hero' );
+?>
+
+
+<div class="<?php if (is_front_page()) { echo 'hero'; } else { echo 'hero hero__subpage'; } ?>">
+	<div class="hero__opacity"></div>
+	<div class="hero__background">
+		<figure class="<?php if (is_front_page()) { echo 'hero__video'; } else { echo 'hero__image'; } ?>">
+			<?php if ( !empty( $hero['background'] ) ) echo $hero['background']; ?>
+		</figure>
+	</div>
+	<div class="hero__text">
+		<div class="hero__container">
+			<h1 class="hero__heading"><?php if ( !empty( $hero['title'] ) ) echo $hero['title']; ?></h1>
+			<div class="hero__description"><?php if ( !empty( $hero['content'] ) ) echo $hero['content']; ?></div>
+		</div>
+	</div>
 </div>
